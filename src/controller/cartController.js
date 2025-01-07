8074a8b0dac5dc3d25589edb4a5f1d567c326498
solomanzaytsev <sulo42sf@gmail.com>
2025-01-07 13:29:02 +0300

    Update cartController.js

--- src/controller/cartController.js
+++ src/controller/cartController.js
@@ -1,8 +1,20 @@
 const Product = require("../model/product");
-const Cart = require("../model/cart")
+const Cart = require("../model/cart");
+const APIError = require("../util/error");
 
 
 const addToCart = async (req, res, next) => {
+  const {productId,quantity} =  req.body;
+  const userId = req.user.id;
+  try {
+    const product = await Product.findById(productId);
+    if(!product){
+      next(new APIError("Ürün Bulunamadı",404));
+    }
+    
+  } catch (error) {
+    
+  }
 
 }
   
